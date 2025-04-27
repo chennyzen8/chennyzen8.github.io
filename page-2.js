15701b48b43e7361dbea9699a6e29be1402840ff
chennyzen8 <zenrye108@gmail.com>
2025-04-28 01:56:01 +0800

    changes

--- page-2.js
+++ page-2.js
@@ -1,26 +1,25 @@
 // MENUS - NAV
 
-const project_tab = document.querySelector(".project-tab")
+const project_tab = document.querySelector(".project-tab");
 
-const project_tab_dropdown = document.querySelector(".project-tab-dropdown")
+const project_tab_dropdown = document.querySelector(".project-tab-dropdown");
 
 project_tab.addEventListener("mouseover", (e) => {
   project_tab_dropdown.style.display = "block";
-  console.log(e)
-})
+  console.log(e);
+});
 
 project_tab.addEventListener("mouseout", (e) => {
   project_tab_dropdown.style.display = "none";
-})
-
+});
 
 // VIDEO - HEADER IMAGE
 
 const video = document.querySelector(".video");
 video.loop = true;
 window.addEventListener("load", (e) => {
-    video.play()
-    console.log(document.querySelector(".title-link"))
+  video.play();
+  console.log(document.querySelector(".title-link"));
 });
 
 const videoPause = () => {
@@ -33,11 +32,11 @@ const videoPlay = () => {
 
 // OPEN & CLOSE LIGHTBOXES
 
-const lightbox_header = document.querySelector(".lightbox-header")
+const lightbox_header = document.querySelector(".lightbox-header");
 
 const open_lightbox_header = (e) => {
   console.log("click");
-//   console.log(e)
+  //   console.log(e)
   lightbox_header.style.display = "block";
 };
 
@@ -45,224 +44,258 @@ const close_lightbox_header = (e) => {
   lightbox_header.style.display = "none";
 };
 
-const lightbox_4 = document.querySelector(".lightbox-4")
+const lightbox_4 = document.querySelector(".lightbox-4");
 
 const open_lightbox_4 = (e) => {
-    console.log("click");
-    lightbox_4.style.display = "block";
+  console.log("click");
+  lightbox_4.style.display = "block";
 };
 
 const close_lightbox_4 = (e) => {
-    lightbox_4.style.display = "none";
+  lightbox_4.style.display = "none";
 };
 
-const lightbox_5__1 = document.querySelector(".lightbox-5__1")
+const lightbox_5__1 = document.querySelector(".lightbox-5__1");
 
 const open_lightbox_5__1 = (e) => {
-    console.log("click");
-    lightbox_5__1.style.display = "block";
+  console.log("click");
+  lightbox_5__1.style.display = "block";
 };
 
 const close_lightbox_5__1 = (e) => {
-    lightbox_5__1.style.display = "none";
+  lightbox_5__1.style.display = "none";
 };
 
-const lightbox_5__2 = document.querySelector(".lightbox-5__2")
+const lightbox_5__2 = document.querySelector(".lightbox-5__2");
 
 const open_lightbox_5__2 = (e) => {
-    console.log("click");
-    lightbox_5__2.style.display = "block";
+  console.log("click");
+  lightbox_5__2.style.display = "block";
 };
 
 const close_lightbox_5__2 = (e) => {
-    lightbox_5__2.style.display = "none";
+  lightbox_5__2.style.display = "none";
 };
 
-const lightbox_5__3 = document.querySelector(".lightbox-5__3")
+const lightbox_5__3 = document.querySelector(".lightbox-5__3");
 
 const open_lightbox_5__3 = (e) => {
-    console.log("click");
-    lightbox_5__3.style.display = "block";
+  console.log("click");
+  lightbox_5__3.style.display = "block";
 };
 
 const close_lightbox_5__3 = (e) => {
-    lightbox_5__3.style.display = "none";
+  lightbox_5__3.style.display = "none";
 };
 
-const lightbox_6__1 = document.querySelector(".lightbox-6")
+const lightbox_6__1 = document.querySelector(".lightbox-6");
 
 const open_lightbox_6__1 = (e) => {
-    console.log("click");
-    lightbox_6__1.style.display = "block";
+  console.log("click");
+  lightbox_6__1.style.display = "block";
 };
 
 const close_lightbox_6__1 = (e) => {
-    lightbox_6__1.style.display = "none";
+  lightbox_6__1.style.display = "none";
 };
-
-
-
 
 // LIGHTBOXES - GALLERY 4
 
-// const l4__1_thriller = ["assets/images/slides/l4__slide-1/thriller-screenshot-4__1_1.png", "url(assets/images/slides/l4__slide-1/thriller-screenshot-4__1_2.png)"]
+const l4__1_thr_photos = 2;
+const l4__1_lnfsg_photos = 1;
 
-// const l4__1_lnfsg = ["assets/images/slides/l4__slide-1/lnfsg-screenshot-4__1_1.png"]
+const l5__1_thr_photos = 1;
+const l5__1_lnfsg_photos = 1;
 
-// const l5__1_thriller = ["assets/images/slides/l5__slide-1/thriller-screenshot-5__1_1.png"]
+const l5__2_thr_photos = 5;
+const l5__2_lnfsg_photos = 7;
 
-// const l5__1_lnfsg = ["assets/images/slides/l5__slide-1/lnfsg-screenshot-5__1_1.png"]
+const l5__3_thr_photos = 5;
+const l5__3_lnfsg_photos = 1;
 
-// const l5__2_thriller = ["assets/images/slides/l5__slide-2/thriller-screenshot-5__2_1.png","assets/images/slides/l5__slide-2/thriller-screenshot-5__2_2.png", "assets/images/slides/l5__slide-2/thriller-screenshot-5__2_3.png"]
+const l6__1_thr_photos = 1;
+const l6__1_lnfsg_photos = 1;
 
-const l4__1_thr_photos = 2  
-const l4__1_lnfsg_photos = 1  
+let l4__1_thriller = [];
+let l4__1_lnfsg = [];
 
-const l5__1_thr_photos = 1  
-const l5__1_lnfsg_photos = 1  
+let l5__1_thriller = [];
+let l5__1_lnfsg = [];
 
-const l5__2_thr_photos = 5
-const l5__2_lnfsg_photos = 7
+let l5__2_thriller = [];
+let l5__2_lnfsg = [];
 
-const l5__3_thr_photos = 5
-const l5__3_lnfsg_photos = 1
+let l5__3_thriller = [];
+let l5__3_lnfsg = [];
 
-const l6__1_thr_photos = 1
-const l6__1_lnfsg_photos = 1
+let l6__1_thriller = [];
+let l6__1_lnfsg = [];
 
-let l4__1_thriller = []
-let l4__1_lnfsg = []
+const fill_photo_array_thr = (
+  number_of_photos,
+  photos_array,
+  gallery_number,
+  slide_number
+) => {
+  let photo_number = 1;
+  while (number_of_photos > 0) {
+    photos_array.push(
+      `assets/images/slides/l${gallery_number}__slide-${slide_number}/thriller-screenshot-${gallery_number}__${slide_number}_${photo_number}.png`
+    );
+    number_of_photos--;
+    photo_number++;
+  }
 
-let l5__1_thriller = []
-let l5__1_lnfsg = []
+  return photos_array;
+};
 
-let l5__2_thriller = []
-let l5__2_lnfsg = []
+const fill_photo_array_lnfsg = (
+  number_of_photos,
+  photos_array,
+  gallery_number,
+  slide_number
+) => {
+  let photo_number = 1;
+  while (number_of_photos > 0) {
+    photos_array.push(
+      `assets/images/slides/l${gallery_number}__slide-${slide_number}/lnfsg-screenshot-${gallery_number}__${slide_number}_${photo_number}.png`
+    );
+    number_of_photos--;
+    photo_number++;
+  }
 
-let l5__3_thriller = []
-let l5__3_lnfsg = []
+  return photos_array;
+};
 
-let l6__1_thriller = []
-let l6__1_lnfsg = []
+l4__1_thriller = fill_photo_array_thr(
+  l4__1_thr_photos,
+  l4__1_thriller,
+  "4",
+  "1"
+);
+l4__1_lnfsg = fill_photo_array_lnfsg(l4__1_lnfsg_photos, l4__1_lnfsg, "4", "1");
 
+l5__1_thriller = fill_photo_array_thr(
+  l5__1_thr_photos,
+  l5__1_thriller,
+  "5",
+  "1"
+);
+l5__1_lnfsg = fill_photo_array_lnfsg(l5__1_lnfsg_photos, l5__1_lnfsg, "5", "1");
 
-const fill_photo_array_thr = (number_of_photos, photos_array, gallery_number, slide_number) => {
-    let photo_number = 1
-    while (number_of_photos > 0) {
-        photos_array.push(`assets/images/slides/l${gallery_number}__slide-${slide_number}/thriller-screenshot-${gallery_number}__${slide_number}_${photo_number}.png`)
-        number_of_photos--
-        photo_number++
-    }
+l5__2_thriller = fill_photo_array_thr(
+  l5__2_thr_photos,
+  l5__2_thriller,
+  "5",
+  "2"
+);
+l5__2_lnfsg = fill_photo_array_lnfsg(l5__2_lnfsg_photos, l5__2_lnfsg, "5", "2");
 
-    return photos_array
-}
+l5__3_thriller = fill_photo_array_thr(
+  l5__3_thr_photos,
+  l5__3_thriller,
+  "5",
+  "3"
+);
+l5__3_lnfsg = fill_photo_array_lnfsg(l5__3_lnfsg_photos, l5__3_lnfsg, "5", "3");
 
-const fill_photo_array_lnfsg = (number_of_photos, photos_array, gallery_number, slide_number) => {
-    let photo_number = 1
-    while (number_of_photos > 0) {
-        photos_array.push(`assets/images/slides/l${gallery_number}__slide-${slide_number}/lnfsg-screenshot-${gallery_number}__${slide_number}_${photo_number}.png`)
-        number_of_photos--
-        photo_number++
-    }
+l6__1_thriller = fill_photo_array_thr(
+  l6__1_thr_photos,
+  l6__1_thriller,
+  "6",
+  "1"
+);
+l6__1_lnfsg = fill_photo_array_lnfsg(l6__1_lnfsg_photos, l6__1_lnfsg, "6", "1");
 
-    return photos_array
-}
+const l4__1_thriller_section = document.querySelector(
+  ".l4__1_thriller-section"
+);
+const l4__1_lnfsg_section = document.querySelector(".l4__1_lnfsg-section");
 
+const l5__1_thriller_section = document.querySelector(
+  ".l5__1_thriller-section"
+);
+const l5__1_lnfsg_section = document.querySelector(".l5__1_lnfsg-section");
 
+const l5__2_thriller_section = document.querySelector(
+  ".l5__2_thriller-section"
+);
+const l5__2_lnfsg_section = document.querySelector(".l5__2_lnfsg-section");
 
-l4__1_thriller = fill_photo_array_thr(l4__1_thr_photos, l4__1_thriller, "4", "1")
-l4__1_lnfsg = fill_photo_array_lnfsg(l4__1_lnfsg_photos, l4__1_lnfsg, "4", "1")
+const l5__3_thriller_section = document.querySelector(
+  ".l5__3_thriller-section"
+);
+const l5__3_lnfsg_section = document.querySelector(".l5__3_lnfsg-section");
 
-l5__1_thriller = fill_photo_array_thr(l5__1_thr_photos, l5__1_thriller, "5", "1")
-l5__1_lnfsg = fill_photo_array_lnfsg(l5__1_lnfsg_photos, l5__1_lnfsg, "5", "1")
+const l6__1_thriller_section = document.querySelector(
+  ".l6__1_thriller-section"
+);
+const l6__1_lnfsg_section = document.querySelector(".l6__1_lnfsg-section");
 
-l5__2_thriller = fill_photo_array_thr(l5__2_thr_photos, l5__2_thriller, "5", "2")
-l5__2_lnfsg = fill_photo_array_lnfsg(l5__2_lnfsg_photos, l5__2_lnfsg, "5", "2")
-
-l5__3_thriller = fill_photo_array_thr(l5__3_thr_photos, l5__3_thriller, "5", "3")
-l5__3_lnfsg = fill_photo_array_lnfsg(l5__3_lnfsg_photos, l5__3_lnfsg, "5", "3")
-
-l6__1_thriller = fill_photo_array_thr(l6__1_thr_photos, l6__1_thriller, "6", "1")
-l6__1_lnfsg = fill_photo_array_lnfsg(l6__1_lnfsg_photos, l6__1_lnfsg, "6", "1")
-
-const l4__1_thriller_section = document.querySelector(".l4__1_thriller-section")
-const l4__1_lnfsg_section = document.querySelector(".l4__1_lnfsg-section")
-
-const l5__1_thriller_section = document.querySelector(".l5__1_thriller-section")
-const l5__1_lnfsg_section = document.querySelector(".l5__1_lnfsg-section")
-
-const l5__2_thriller_section = document.querySelector(".l5__2_thriller-section")
-const l5__2_lnfsg_section = document.querySelector(".l5__2_lnfsg-section")
-
-const l5__3_thriller_section = document.querySelector(".l5__3_thriller-section")
-const l5__3_lnfsg_section = document.querySelector(".l5__3_lnfsg-section")
-
-const l6__1_thriller_section = document.querySelector(".l6__1_thriller-section")
-const l6__1_lnfsg_section = document.querySelector(".l6__1_lnfsg-section")
-
-const l4__1_first_slide = document.querySelector(".l4__first-slide")
-const l5__1_first_slide = document.querySelector(".l5__1_first-slide")
-const l5__2_first_slide = document.querySelector(".l5__2_first-slide")
-const l5__3_first_slide = document.querySelector(".l5__3_first-slide")
-const l6__1_first_slide = document.querySelector(".l6__1_first-slide")
+const l4__1_first_slide = document.querySelector(".l4__first-slide");
+const l5__1_first_slide = document.querySelector(".l5__1_first-slide");
+const l5__2_first_slide = document.querySelector(".l5__2_first-slide");
+const l5__3_first_slide = document.querySelector(".l5__3_first-slide");
+const l6__1_first_slide = document.querySelector(".l6__1_first-slide");
 
 const add_photos = (gallery_no__mv, photos_array, first_slide) => {
-    photos_array.forEach(photo_url => {
-        const new_photo = document.createElement("div")
-        new_photo.classList.add(photos_array[0].includes("thriller") ? "photo-container-thr__small" : "photo-container")
-        new_photo.style.backgroundImage = `url(${photo_url})`
-        gallery_no__mv.append(new_photo)
-        new_photo.addEventListener("click", (e) => {
-            first_slide.style.backgroundImage = `url(${photo_url})`
-            console.log('should change')
-        })  
+  photos_array.forEach((photo_url) => {
+    const new_photo = document.createElement("div");
+    new_photo.classList.add(
+      photos_array[0].includes("thriller")
+        ? "photo-container-thr__small"
+        : "photo-container"
+    );
+    new_photo.style.backgroundImage = `url(${photo_url})`;
+    gallery_no__mv.append(new_photo);
+    new_photo.addEventListener("click", (e) => {
+      first_slide.style.backgroundImage = `url(${photo_url})`;
+      console.log("should change");
     });
-}
+  });
+};
 
-add_photos(l4__1_thriller_section, l4__1_thriller, l4__1_first_slide)
-add_photos(l4__1_lnfsg_section, l4__1_lnfsg, l4__1_first_slide)
+add_photos(l4__1_thriller_section, l4__1_thriller, l4__1_first_slide);
+add_photos(l4__1_lnfsg_section, l4__1_lnfsg, l4__1_first_slide);
 
-add_photos(l5__1_thriller_section, l5__1_thriller, l5__1_first_slide)
-add_photos(l5__1_lnfsg_section, l5__1_lnfsg, l5__1_first_slide)
+add_photos(l5__1_thriller_section, l5__1_thriller, l5__1_first_slide);
+add_photos(l5__1_lnfsg_section, l5__1_lnfsg, l5__1_first_slide);
 
-add_photos(l5__2_thriller_section, l5__2_thriller, l5__2_first_slide)
-add_photos(l5__2_lnfsg_section, l5__2_lnfsg, l5__2_first_slide)
+add_photos(l5__2_thriller_section, l5__2_thriller, l5__2_first_slide);
+add_photos(l5__2_lnfsg_section, l5__2_lnfsg, l5__2_first_slide);
 
-add_photos(l5__3_thriller_section, l5__3_thriller, l5__3_first_slide)
-add_photos(l5__3_lnfsg_section, l5__3_lnfsg, l5__3_first_slide)
+add_photos(l5__3_thriller_section, l5__3_thriller, l5__3_first_slide);
+add_photos(l5__3_lnfsg_section, l5__3_lnfsg, l5__3_first_slide);
 
-add_photos(l6__1_thriller_section, l6__1_thriller, l6__1_first_slide)
-add_photos(l6__1_lnfsg_section, l6__1_lnfsg, l6__1_first_slide)
+add_photos(l6__1_thriller_section, l6__1_thriller, l6__1_first_slide);
+add_photos(l6__1_lnfsg_section, l6__1_lnfsg, l6__1_first_slide);
 
-const l4__1_finished_edit = document.querySelector(".l4__finished-edit")
-const l5__1_finished_edit = document.querySelector(".l5__1_finished-edit")
-const l5__2_finished_edit = document.querySelector(".l5__2_finished-edit")
-const l5__3_finished_edit = document.querySelector(".l5__3_finished-edit")
-const l6__1_finished_edit = document.querySelector(".l6__1_finished-edit")
+const l4__1_finished_edit = document.querySelector(".l4__finished-edit");
+const l5__1_finished_edit = document.querySelector(".l5__1_finished-edit");
+const l5__2_finished_edit = document.querySelector(".l5__2_finished-edit");
+const l5__3_finished_edit = document.querySelector(".l5__3_finished-edit");
+const l6__1_finished_edit = document.querySelector(".l6__1_finished-edit");
 
 l4__1_finished_edit.addEventListener("click", (e) => {
-    console.log("should change!")
-    l4__1_first_slide.style.backgroundImage = "url(assets/images/gl-image-4.png)"
-})
+  console.log("should change!");
+  l4__1_first_slide.style.backgroundImage = "url(assets/images/gl-image-4.png)";
+});
 
 l5__1_finished_edit.addEventListener("click", (e) => {
-    l5__1_first_slide.style.backgroundImage = "url(assets/images/gl-images-5__1.png)"
-})
+  l5__1_first_slide.style.backgroundImage =
+    "url(assets/images/gl-images-5__1.png)";
+});
 
 l5__2_finished_edit.addEventListener("click", (e) => {
-    l5__2_first_slide.style.backgroundImage = "url(assets/images/gl-images-5__2.png)"
-})
+  l5__2_first_slide.style.backgroundImage =
+    "url(assets/images/gl-images-5__2.png)";
+});
 
 l5__3_finished_edit.addEventListener("click", (e) => {
-    l5__3_first_slide.style.backgroundImage = "url(assets/images/gl-images-5__3.png)"
-})
+  l5__3_first_slide.style.backgroundImage =
+    "url(assets/images/gl-images-5__3.png)";
+});
 
 l6__1_finished_edit.addEventListener("click", (e) => {
-    l6__1_first_slide.style.backgroundImage = "url(assets/images/gl-image-6.png)"
-})
-
-
-// // SCROLL AT HOVER
-
-// const edited_photos_section = document.querySelector(".edited-photos-section")
-
+  l6__1_first_slide.style.backgroundImage = "url(assets/images/gl-image-6.png)";
+});
